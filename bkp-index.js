da5ee662a65b825cb9460906d46b67914cb3337d
ivansansao <aivanplus@gmail.com>
2022-01-28 13:57:05 -0300

    Adicionei movimento de volante às rodas dianteiras

---
+++ bkp-index.js
@@ -0,0 +1,88 @@
+
+function mousePressed() {
+}
+
+function keyPressed() {
+    if (keyCode == RIGHT_ARROW) {
+        car.setRotation(0.1);
+    } else if (keyCode == LEFT_ARROW) {
+        car.setRotation(-0.1);
+    } else if (keyCode == UP_ARROW) {
+        car.boost();
+    }
+}
+
+function keyReleased() {
+    car.setRotation(0);
+}
+
+
+let car = null;
+function setup() {
+    createCanvas(windowWidth, windowHeight*0.99);
+    car = new Car();
+}
+function draw() {
+
+    background(0,200);
+
+    car.update();
+    car.turn();
+    car.render();
+
+
+}
+// https://github.com/davenewt/p5-asteroids/blob/master/ship.js
+class Car {
+
+    constructor() {
+        this.pos = createVector(width / 2, height / 2);
+        this.heading = 0;
+        this.headingV = p5.Vector.fromAngle(radians(this.heading));
+        this.rotation = 0;
+        this.vel = createVector(0,0);
+    }
+
+    setRotation(a) {
+        this.rotation = a;
+    }
+
+    boost() {
+        var force = p5.Vector.fromAngle(this.heading);
+        this.vel.add(force);
+    }
+
+    turn() {
+
+        this.heading += this.rotation;
+        this.headingV = p5.Vector.fromAngle(radians(this.heading));
+        
+    }
+    update() {
+       
+        this.pos.add(this.vel);
+        this.vel.mult(0.99); // Desacelera a cada update.
+        noStroke();
+        text(`${this.pos}`,50,50);
+        
+    }
+    
+    render() {
+        
+        strokeWeight(2);
+        stroke(255);
+        fill(0,200,0);
+        push();
+            translate(this.pos.x, this.pos.y);
+            // line(-200,0,200,0);
+            // line(0,-200,0,200);
+            rotate(this.heading);
+            rect(-20,-10,40,20);
+            strokeWeight(8);
+            point(20,0);
+        pop();
+
+
+    }
+
+}
---
+++ main.js
@@ -0,0 +1,179 @@
+// https://github.com/davenewt/p5-asteroids/blob/master/ship.js
+
+let cars = [];
+
+function setup() {
+
+    createCanvas(windowWidth, windowHeight * 0.98);
+
+    for (let i = 0; i < 100; i++) {
+        cars.push(new Car());
+    }
+
+}
+
+function draw() {
+
+    background(255);
+
+    for (let i = 0; i < cars.length; i++) {
+        if (keyIsDown(UP_ARROW)) {
+            cars[i].vaiPraFrente();
+        } else if (keyIsDown(DOWN_ARROW)) {
+            cars[i].vaiPraTras();
+        }
+        if (keyIsDown(RIGHT_ARROW)) {
+            cars[i].vaiPraDireita();
+        } else if (keyIsDown(LEFT_ARROW)) {
+            cars[i].vaiPraEsquerda();
+        }
+        cars[i].update();
+        cars[i].demo();
+        cars[i].show();
+    }
+
+}
+
+class Car {
+
+    constructor() {
+
+        this.pos = createVector(width * 0.5, height * 0.5);
+        this.heading = random(0, PI * 2);
+        this.rotation = 0;
+        this.marcha = 0;
+        this.cor = 'hsla(' + Math.floor(Math.random() * 360) + ',100%,50%,0.3)'
+        this.volanteAngle = '';
+        this.demoLado = '';
+
+    }
+
+    vaiPraFrente() {
+        this.marcha = 1;
+    }
+    vaiPraTras() {
+        this.marcha = -1;
+    }
+
+    vaiPraDireita() {
+        if (this.marcha > 0)
+            this.rotation = 0.1;
+        else if (this.marcha < 0)
+            this.rotation = -0.1;
+
+        this.volanteAngle = 'r';
+    }
+    vaiPraEsquerda() {
+        if (this.marcha > 0)
+            this.rotation = -0.1;
+        else if (this.marcha < 0)
+            this.rotation = 0.1;
+
+        this.volanteAngle = 'l';
+    }
+
+    update() {
+
+        this.heading += this.rotation * 0.3; // 0.3 é o quanto o veiculo esterce
+
+        let irPara = p5.Vector.fromAngle(this.heading).mult(3).mult(this.marcha);
+
+        this.pos.add(irPara);
+
+        this.marcha = 0;
+        this.rotation = 0;
+
+    }
+
+    show() {
+
+        push();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.heading);
+        this.drawCar();
+        pop();
+        this.volanteAngle = '';
+
+    }
+
+    drawCar() {
+        strokeWeight(2);
+        stroke(0);
+
+        fill(0);
+        rect(-6, -15, 6, 4, 1); // Roda traseira
+        rect(-6, 11, 6, 4, 1); // Roda traseira
+
+        push();
+        translate(23, -12);
+        if (this.volanteAngle == 'l') rotate(-0.3);
+        if (this.volanteAngle == 'r') rotate(0.3);
+        rect(-3, -2, 6, 4, 1); // Roda dianteira
+        pop();
+
+        push();
+        translate(23, 12);
+        if (this.volanteAngle == 'l') rotate(-0.3);
+        if (this.volanteAngle == 'r') rotate(0.3);
+        rect(-3, -2, 6, 4, 1); // Roda dianteira
+        pop();
+        
+        fill(this.cor);
+        rect(-8, -10, 40, 20, 5);
+  
+        strokeWeight(6);
+        stroke(255);
+        point(28, -6);
+        point(28, 6);
+    }
+
+    demo() {
+
+        let movimento = Number((Math.random() * (100 - 1)).toFixed(0));
+        this.marcha = 1
+
+        if (movimento > 30) {
+            this.vaiPraFrente();        
+        }
+
+        if (frameCount % 50 == 0) {
+
+            const lado = Math.floor(random(0.0,0.4)*10)
+
+            if (lado == 0) {
+                this.demoLado = ''
+            } else if (lado == 1) {
+                this.demoLado = 'r'
+            } else if (lado == 2) {
+                this.demoLado = 'l'
+            }
+        }
+
+
+        if (this.demoLado == 'r')
+            this.vaiPraDireita();
+        else if (this.demoLado == 'l')
+            this.vaiPraEsquerda();
+
+        if (this.pos.x > width)
+            this.pos.x = 0;
+
+        if (this.pos.x < 0)
+            this.pos.x = width;
+
+        if (this.pos.y > height)
+            this.pos.y = 0;
+
+        if (this.pos.y < 0)
+            this.pos.y = height;
+    }
+
+}
+
+function linha() {
+    stroke(0);
+    line(-width, 0, width, 0);
+    line(0, -height, 0, height);
+}
+
+
